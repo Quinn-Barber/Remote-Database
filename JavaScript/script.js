48505b7444c46375d5e024954be364ff9a4e56b6
Marco Peric <mperic@knights.ucf.edu>
2022-09-07 19:14:18 -0400

    foundational code for LAMPAPI.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,6 +1,58 @@
+const urlBase = 'http://poostproject.xyz/LAMPAPI';
+const extension = 'php';
+
 const form = document.querySelector("body"),
-    pwShowHide = document.querySelectorAll(".eye-icon");
+pwShowHide = document.querySelectorAll(".eye-icon");
     
+function doLogin()
+{
+    userId = 0;
+    firstName = "";
+    lastName = "";
+    
+    let login = document.getElementById("loginName").value;
+    let password = document.getElementById("loginPassword").value;
+    document.getElementById("loginResult").innerHTML = "";
+
+    let tmp = {login:login,password:password};
+    let jsonPayload = JSON.stringify(tmp);
+    let url = urlBase + '/Login.' + extension;
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    try
+    {
+        xhr.onreadystatechange = function() 
+        {
+            if (this.readyState == 4 && this.status == 200) 
+            {
+                let jsonObject = JSON.parse(xhr.responseText);
+                userId = jsonObject.id;
+        
+                if( userId < 1 )
+                {		
+                    document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+                    return;
+                }
+        
+                firstName = jsonObject.firstName;
+                lastName = jsonObject.lastName;
+
+                saveCookie();
+    
+                window.location.href = "color.html";
+            }
+        };
+
+        xhr.send(jsonPayload);
+    }
+    catch(err)
+    {
+        document.getElementById("loginResult").innerHTML = err.message;
+    }
+
+}
 
 pwShowHide.forEach(eyeIcon => {
     eyeIcon.addEventListener("click", () => {

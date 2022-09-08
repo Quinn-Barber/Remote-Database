a5714622f047eadbd588664a1a144d1aa9e09dd3
Marco Peric <mperic@knights.ucf.edu>
2022-09-08 15:51:41 -0400

    .

--- LAMPAPI/create_account.php
+++ LAMPAPI/create_account.php
@@ -1,18 +1,26 @@
 <?php
+
+	error_reporting(E_ALL);
+	ini_set('display_errors', 'on');
+
 	$inData = getRequestInfo();
 	
-	$color = $inData["username"];
-	$userId = $inData["password"];
+	$uname = $inData["username"];
+	$pw = $inData["password"];
+	$fname = $inData["firstName"];
+	$lname = $inData["lastName"];
+	$pnum = $inData["phoneNumber"];
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+    // FIXME
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "contact_list_app_db");
 	if ($conn->connect_error) 
 	{
-		returnWithError( $conn->connect_error );
+		returnWithError($conn->connect_error);
 	} 
 	else
 	{
-		$stmt = $conn->prepare("INSERT into Colors (UserId,Name) VALUES(?,?)");
-		$stmt->bind_param("ss", $userId, $color);
+		$stmt = $conn->prepare("INSERT into user_list (user_id, username, password, firstname, lastname) VALUES(?,?,?,?,?)");
+		$stmt->bind_param("issss", 0, $uname, $pw, $fname, $lname);
 		$stmt->execute();
 		$stmt->close();
 		$conn->close();
@@ -24,16 +32,16 @@
 		return json_decode(file_get_contents('php://input'), true);
 	}
 
-	function sendResultInfoAsJson( $obj )
+	function sendResultInfoAsJson($obj)
 	{
 		header('Content-type: application/json');
 		echo $obj;
 	}
 	
-	function returnWithError( $err )
+	function returnWithError($err)
 	{
 		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
+		sendResultInfoAsJson($retValue);
 	}
 	
 ?>

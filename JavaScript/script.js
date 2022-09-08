8224eab4d3dd81ded16bd8e8d58f7afc9df6f5af
Marco Peric <mperic@knights.ucf.edu>
2022-09-08 01:20:05 -0400

    restarting lampapi from leinecker's code

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,128 +1,66 @@
-const urlBase = 'http://67.205.182.117/LAMPAPI'; // http://poostproject.xyz/LAMPAPI // 'http://67.205.182.117/LAMPAPI';
+const urlBase = 'http://67.205.182.117/LAMPAPI';
 const extension = 'php';
 
-const form = document.querySelector("body"),
-pwShowHide = document.querySelectorAll(".eye-icon");
-
-function doCreate()
-{    
-    let username = document.getElementById("registerUsername").value;
-    let password = document.getElementById("registerPassword").value;
-    let firstName = document.getElementById("registerFirstName").value;
-    let lastName =  document.getElementById("registerLastName").value;
-    let phoneNumber = document.getElementById("registerPhoneNumber").value;
-
-    // document.getElementById("loginResult").innerHTML = "";
-
-    let tmp = {username:username, password:password, firstName:firstName, lastName:lastName, phoneNumber:phoneNumber};
-    let jsonPayload = JSON.stringify(tmp);
-    console.log(jsonPayload);
-    let url = urlBase + '/create_account.' + extension;
-
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-    try
-    {
-        xhr.onreadystatechange = function() 
-        {
-            if (this.readyState == 4 && this.status == 200) 
-            {
-                let jsonObject = JSON.parse(xhr.responseText);
-                console.log(jsonObject);
-                userId = jsonObject.id;
-        
-                if(userId < 1) // ???
-                {		
-                    // document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
-                    return;
-                }
-        
-                // firstName = jsonObject.firstName;
-                // lastName = jsonObject.lastName;
-
-                saveCookie();
-
-                window.location.href = "forgotpass.html";
-            }
-        };
-
-        xhr.send(jsonPayload);
-
-    }catch(err)
-    {
-        console.log("error");
-    }
-}
+let userId = 0;
+let firstName = "";
+let lastName = "";
+// SamH
+// Test
 
 function doLogin()
 {
-    userId = 0;
-    firstName = "";
-    lastName = "";
-    
-    let login = document.getElementById("loginName").value;
-    let password = document.getElementById("loginPassword").value;
-    document.getElementById("loginResult").innerHTML = "";
+	userId = 0;
+	firstName = "";
+	lastName = "";
+	
+	let login = document.getElementById("loginName").value;
+	let password = document.getElementById("loginPassword").value;
+//	var hash = md5( password );
+	
+	document.getElementById("loginResult").innerHTML = "";
 
-    let tmp = {login:login,password:password};
-    let jsonPayload = JSON.stringify(tmp);
-    let url = urlBase + '/login.' + extension;
+	let tmp = {login:login,password:password};
+//	var tmp = {login:login,password:hash};
+	let jsonPayload = JSON.stringify( tmp );
+	
+	let url = urlBase + '/Login.' + extension;
 
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true); // should this be GET?
-    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-    try
-    {
-        xhr.onreadystatechange = function() 
-        {
-            if (this.readyState == 4 && this.status == 200) 
-            {
-                let jsonObject = JSON.parse(xhr.responseText);
-                console.log(jsonObject); // print out the contents of the response.
-                userId = jsonObject.id;
-        
-                if( userId < 1 )
-                {		
-                    document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
-                    return;
-                }
-        
-                firstName = jsonObject.firstName;
-                lastName = jsonObject.lastName;
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				console.log(xhr.responseText);
+				let jsonObject = JSON.parse( xhr.responseText );
+				console.log(jsonObject);
+				userId = jsonObject.id;
+		
+				if( userId < 1 )
+				{		
+					document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+					return;
+				}
+		
+				firstName = jsonObject.firstName;
+				lastName = jsonObject.lastName;
 
-                saveCookie();
-    
-                window.location.href = "color.html";
-            }
-        };
-
-        xhr.send(jsonPayload);
-    }
-    catch(err)
-    {
-        document.getElementById("loginResult").innerHTML = err.message;
-    }
+				saveCookie();
+	
+				window.location.href = "color.html";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("loginResult").innerHTML = err.message;
+	}
 
 }
-
-pwShowHide.forEach(eyeIcon => {
-    eyeIcon.addEventListener("click", () => {
-        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".loginPassword");
-        
-        console.log(pwFields);
-
-        pwFields.forEach(loginPassword => {
-            if(loginPassword.type === "password"){
-                loginPassword.type = "text";
-                eyeIcon.classList.replace("bx-show", "bx-hide");
-                return;
-            }
-            loginPassword.type = "password";
-            eyeIcon.classList.replace("bx-hide", "bx-show");
-        })
-    })
-})
 
 function saveCookie()
 {
@@ -163,4 +101,89 @@ function readCookie()
 	{
 		document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
 	}
+}
+
+function doLogout()
+{
+	userId = 0;
+	firstName = "";
+	lastName = "";
+	document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+	window.location.href = "index.html";
+}
+
+function addColor()
+{
+	let newColor = document.getElementById("colorText").value;
+	document.getElementById("colorAddResult").innerHTML = "";
+
+	let tmp = {color:newColor,userId,userId};
+	let jsonPayload = JSON.stringify( tmp );
+
+	let url = urlBase + '/AddColor.' + extension;
+	
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				document.getElementById("colorAddResult").innerHTML = "Color has been added";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("colorAddResult").innerHTML = err.message;
+	}
+	
+}
+
+function searchColor()
+{
+	let srch = document.getElementById("searchText").value;
+	document.getElementById("colorSearchResult").innerHTML = "";
+	
+	let colorList = "";
+
+	let tmp = {search:srch,userId:userId};
+	let jsonPayload = JSON.stringify( tmp );
+
+	let url = urlBase + '/SearchColors.' + extension;
+	
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				document.getElementById("colorSearchResult").innerHTML = "Color(s) has been retrieved";
+				let jsonObject = JSON.parse( xhr.responseText );
+				
+				for( let i=0; i<jsonObject.results.length; i++ )
+				{
+					colorList += jsonObject.results[i];
+					if( i < jsonObject.results.length - 1 )
+					{
+						colorList += "<br />\r\n";
+					}
+				}
+				
+				document.getElementsByTagName("p")[0].innerHTML = colorList;
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("colorSearchResult").innerHTML = err.message;
+	}
+	
 }

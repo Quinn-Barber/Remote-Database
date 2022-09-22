3c91413d7f2a16047f82d1efc9c3ba23e19d2911
Marco Peric <mperic@knights.ucf.edu>
2022-09-22 01:22:17 -0400

    stringify JSON

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -435,7 +435,7 @@ function saveCookie()
 	let date = new Date();
 	date.setTime(date.getTime()+(minutes*60*1000));	
 	document.cookie = "firstName=" + firstName + "#lastName=" + lastName + "#userId=" + userId + 
-					  "#editing=" + editing + "#modified=" + modified + "#expires=" + date.toGMTString();
+					  "#editing=" + editing + "#modified=" + JSON.stringify(modified) + "#expires=" + date.toGMTString();
 }
 
 function readCookie()

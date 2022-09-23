4ea42f515b065aae1b7f4405ad187435e8823fca
Marco Peric <mperic@knights.ucf.edu>
2022-09-22 23:31:17 -0400

    logout

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -505,7 +505,7 @@ function doLogout()
 	lastName = "";
 	modified = {};
 	document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
-	window.location.href = "index.html";
+	window.location.href = "../index.html";
 }
 
 // function searchColor()
